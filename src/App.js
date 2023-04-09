d6ca620ca0ade3a0a930bf6befa9f922cc2cbce3
John_Galt <john.galt.prog@gmail.com>
2023-04-09 19:39:31 +0300

    include Render-props

--- src/App.js
+++ src/App.js
@@ -102,9 +102,46 @@ const HelloGreating = () => {
 	)
 }
 
+const Message = (props) => {
+	return (
+		<h2>The counter is {props.counter}</h2>
+	)
+}
+
+class Counter extends Component {
+	state = {
+		counter: 0
+	}
+
+	changeCounter = () => {
+		this.setState(({counter}) => ({
+			counter: counter + 1
+		}))
+	}
+
+	render() {
+		return (
+			<>
+				<button
+					className={'btn btn-primary'}
+					onClick={this.changeCounter}>
+					Click me
+				</button>
+				{/* <Message counter={this.state.counter}/> */}
+				{this.props.render(this.state.counter)}
+			</>
+		)
+	}
+}
+
 function App() {
   return (
     <Wrapper>
+
+		<Counter render={counter => (
+			<Message counter={counter}/>
+		)}/>
+
 		<HelloGreating/>
 		<DynamicGreating color={'primary'}>
 			<h2>This weel was hard</h2>
